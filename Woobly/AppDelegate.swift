ac436da9d7200a0a353300a0b407e46f53468692
Myrenkar <piotr.torczynski@gmail.com>
2017-08-18 11:50:25 +0200

    improvements in Appdelegate

--- Woobly/AppDelegate.swift
+++ Woobly/AppDelegate.swift
@@ -12,8 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    private lazy var blurEffect = UIVisualEffectView(effect: UIBlurEffect(style: .light))
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let navigationController = UINavigationController()
@@ -37,19 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        blurEffect.frame = UIScreen.main.bounds
-        window?.addSubview(blurEffect)
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        blurEffect.removeFromSuperview()
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         if url.absoluteString.contains("real_password=ai3zn2jza") {
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "Activated"), object: nil, userInfo: nil)
